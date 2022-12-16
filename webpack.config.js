c9afdac2eb612d3287bfd9ecb868cf9259896640
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2022-12-16 10:19:56 +0100

    Create 'webpack.config.js' file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
