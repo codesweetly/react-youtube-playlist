9606631f26c5cba5df7958cb657dce2cd99e59de
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2022-12-16 11:46:30 +0100

    Install '@babel/core'

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,10 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
       },
     ],
   },
