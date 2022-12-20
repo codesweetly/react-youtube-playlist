dabebb3bf5d870bf8e9abced995fa58935ceab4f
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2022-12-20 09:35:00 +0100

    Ignore 'dist' directory

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,13 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
+            presets: [
+              "@babel/preset-env",
+              "@babel/react",
+              {
+                plugins: ["@babel/plugin-proposal-class-properties"],
+              },
+            ],
           },
         },
       },
