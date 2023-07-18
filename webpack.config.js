36e2d136b3ca9ccc3fe6441c42f0091a4ae81936
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-07-18 18:53:42 +0100

    chore: Configure webpack to bundle typescript files

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ module.exports = {
   module: {
     rule: [
       {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }],
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
         test: /\.vanilla\.css$/i,
         use: [
           MiniCssExtractPlugin.loader,
