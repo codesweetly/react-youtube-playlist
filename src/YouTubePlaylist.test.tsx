fe691baa34646b4a1283e49dec46fe7d37d2ef56
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2023-07-19 16:06:21 +0100

    test: Remove jest configurations

--- src/YouTubePlaylist.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import YouTubePlaylist from "./YouTubePlaylist";
-
-test("youtube playlist renders correctly", () => {
-  render(<YouTubePlaylist apiKey="" playlistId="" uniqueName="" />);
-});
